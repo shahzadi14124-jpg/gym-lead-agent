1fa27f724cbf72a885d4f72a089be06dd2857e8f
shahzadi14124-jpg <shahzadi14124@gmail.com>
2026-04-14 20:15:46 +0530

    Update redisClient.ts

--- src/queues/redisClient.ts
+++ src/queues/redisClient.ts
@@ -2,7 +2,7 @@ import Redis from 'ioredis';
 
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 
-// One single, shared connection for everything - much more stable!
+// ONE single connection for everything - much more stable!
 export const redisConnection = new Redis(REDIS_URL, {
   maxRetriesPerRequest: null,
   enableReadyCheck: false,
